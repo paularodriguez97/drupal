510b25f9a3124b2b6f61942047fb88e007facb68
Valen <valerodri_97@hotmail.com>
2022-08-02 10:00:53 -0500

    avance modulo

---
+++ modules/custom/domain_redirect/README.md
@@ -0,0 +1,16 @@
+[[_TOC_]]
+
+#### Introduction
+
+The domain redirection module, as its name suggests, redirects a group to a domain. This domain functionality already exists in the Domain module, on which this module depends. The difference lies in that by this module when clicking directly on the group link or in the group view option, it redirects us to the selected domain, that is to say, directly from the current site we can access the other domain through the group links.
+
+#### Requirements
+
+Module requirements:
+- The Groups and Domain module must be active and previously configured.
+- It depends on the module Domain group
+
+#### Maintainers
+
+Seed Em:
+https://www.drupal.org/seed-em
---
+++ modules/custom/domain_redirect/src/EventSubscriber/GroupDomainRedirect.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\domain_redirect\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\Event;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\domain\Entity\Domain;
+
+
+
+/**
+ * Class GroupDomainRedirect.
+ */
+class GroupDomainRedirect implements EventSubscriberInterface {
+
+  /**
+   *
+   */
+  public function groupDomainRedirect(GetResponseEvent $event) {
+    $request = $event->getRequest();
+
+    // Verifica si la ruta es la de un grupo
+    if ($request->attributes->get('_route') !== 'entity.group.canonical') {
+        return;
+    }
+    // Obtiene el hostname actual
+    $host = $request->getHost();
+    // Obtiene el ID del grupo
+    $id = $request->attributes->get('group')->id();
+    // Se carga la información del dominio de este grupo
+    $domain = Domain::load('group_' . $id);
+    // Solo si hay un dominio asociado
+    if($domain){
+      $hostname = $domain->getHostname();
+      // Siempre y cuando el hostname de la petición sea diferente al del redireccionamiento
+      if($host != $hostname){
+        $response = new TrustedRedirectResponse('http://'.$hostname);
+        $event->setResponse($response);
+      }
+    }
+
+
+    return;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+
+    $events[KernelEvents::REQUEST][] = ['groupDomainRedirect',30];
+    return $events;
+
+  }
+
+}
